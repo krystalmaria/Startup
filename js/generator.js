126f9f527c4358b2793b74a2094e641ddda64e9c
Krystal <kf@papercloud.com.au>
2014-02-02 18:30:12 +1100

    Unbroke the generator.

--- js/generator.js
+++ js/generator.js
@@ -15,6 +15,55 @@ function randomArray(a) {
 return a[Math.floor(Math.random() * a.length)];
 }
 
+function runGenerator() {
+
+	var adjective = randomArray(adjectivesArray);
+	var noun = randomArray(nounsArray);
+
+	function getmodifier() {
+		var modifier = Math.random();
+		if (modifier < 0.3) {
+			return randomArray(modifiersArray);
+		} else {
+			return "";
+		}
+	}	
+
+	var modifier = getmodifier();
+
+	var output = (adjective + " " + noun + " " + modifier);
+
+	$(".element").share({
+		text: "I just found my new startup idea: " + output,
+		app_id: 1448143845416014,
+		background: "#B2D1E5",
+		color: "#3C8DC5",
+		button_text: "Share this startup idea!",
+
+		facebook: {
+		  name: "What's my startup idea?",
+		  link: "http://krystalfister.github.io/Startup/",
+		  image: "http://krystalfister.github.io/Startup/images/logo.png",
+		  caption: "I just found my new startup idea: " + output,
+		  text: "Randomly generate ideas for your startup."
+		},
+
+		twitter: {
+		  text: "I just found my new startup idea: " + output,
+		  link: "http://krystalfister.github.io/Startup/"
+		},
+
+		gplus: {
+			link: "http://krystalfister.github.io/Startup/"
+		}
+	});
+
+
+	$("#output").html("<p>"+output+"</p>");
+	return output;
+
+}
+
 function sentence() {
 
 	// Check that json has loaded before running generator
@@ -28,54 +77,5 @@ function sentence() {
 
 	} else {
 		runGenerator();
-	}
-
-	function runGenerator() {
-
-		var adjective = randomArray(adjectivesArray);
-		var noun = randomArray(nounsArray);
-
-		function getmodifier() {
-			var modifier = Math.random();
-			if (modifier < 0.3) {
-				return randomArray(modifiersArray);
-			} else {
-				return "";
-			}
-		}	
-
-		var modifier = getmodifier();
-
-		var output = (adjective + " " + noun + " " + modifier);
-
-		$(".element").share({
-			text: "I just found my new startup idea: " + output,
-			app_id: 1448143845416014,
-			background: "#B2D1E5",
-			color: "#3C8DC5",
-			button_text: "Share this startup idea!",
-
-			facebook: {
-			  name: "What's my startup idea?",
-			  link: "http://krystalfister.github.io/Startup/",
-			  image: "http://krystalfister.github.io/Startup/images/logo.png",
-			  caption: "I just found my new startup idea: " + output,
-			  text: "Randomly generate ideas for your startup."
-			},
-
-			twitter: {
-			  text: "I just found my new startup idea: " + output,
-			  link: "http://krystalfister.github.io/Startup/"
-			},
-
-			gplus: {
-				link: "http://krystalfister.github.io/Startup/"
-			}
-		});
-
-
-		$("#output").html("<p>"+output+"</p>");
-		return output;
-
 	}
 }
